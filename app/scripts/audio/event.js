2354e389ff814790154116a6310eca00a940ae7b
exerinity <me@ex3.lol>
2026-03-10 16:37:28 +1100

    m

---
+++ app/scripts/audio/event.js
@@ -0,0 +1,16 @@
+elements.player.addEventListener('pause', () => {
+    document.getElementById('plps').innerHTML = '<i class="fa-solid fa-play"></i>';
+    stat_up('<i class="fa-solid fa-circle-pause"></i> Paused playback');
+    if ('mediaSession' in navigator) {
+        try { navigator.mediaSession.playbackState = 'paused'; } catch { }
+    }
+});
+
+elements.player.addEventListener('play', () => {
+    stallExit = true;
+    document.getElementById('plps').innerHTML = '<i class="fa-solid fa-pause"></i>';
+    stat_up('<i class="fa-solid fa-circle-play"></i> Resumed playback');
+    if ('mediaSession' in navigator) {
+        try { navigator.mediaSession.playbackState = 'playing'; } catch { }
+    }
+});
