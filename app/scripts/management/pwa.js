be98882cece35a83bc515d71796bfca4b0f5ff2a
exerinity <me@ex3.lol>
2026-03-10 16:37:29 +1100

    m

---
+++ app/scripts/management/pwa.js
@@ -0,0 +1,61 @@
+var uptodate = null;
+
+// If ?noinstall is present, do NOT install the service worker
+const noInstall = new URLSearchParams(location.search).has('noinstall');
+
+if ('serviceWorker' in navigator && !noInstall) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js');
+
+      uptodate = true;
+
+      if (registration.waiting) {
+        if (typeof throw_error === 'function') {
+          throw_error('A new version is ready to install, <a href="#" onclick="window.location.href=window.location.href.split(\'?\')[0]+\'?cachebuster=\'+Date.now();return false;">please refresh the app to finish</a>', true);
+        }
+        uptodate = false;
+      }
+
+      registration.addEventListener('updatefound', () => {
+        const nw = registration.installing;
+        if (!nw) {
+          uptodate = true;
+          return;
+        }
+
+        nw.addEventListener('statechange', () => {
+          stat_up('<i class="fa-solid fa-download fa-fade"></i> The Service Worker is updating...');
+          if (nw.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              if (typeof throw_error === 'function') {
+                stat_up('<i class="fa-solid fa-download"></i> The Service Worker was updated');
+                msg(
+                  'There is a new version of Voxity available! ' +
+                  'Please <a href="#" onclick="window.location.href=window.location.href.split(\'?\')[0]+\'?cachebuster=\'+Date.now();return false;">refresh the app</a> to update - or to ensure it <i>actually</i> updates, click that hyperlink.<br>' +
+                  '<hr><a href="/i/release_notes" onclick="event.preventDefault(); relnote()">View release notes</a><br>' +
+                  '<small>Or, if you don\'t want to, just close this box, I won\'t force you...</small>'
+                );
+              }
+              uptodate = false;
+            } else {
+              if (typeof throw_error === 'function') {
+                stat_up('<i class="fa-solid fa-check"></i> Service Worker finished updating');
+                throw_error('Ready to work offline', true);
+              }
+              uptodate = true;
+            }
+          }
+        });
+      });
+    } catch (error) {
+      console.error('SW registration failed:', error);
+      if (typeof throw_error === 'function') {
+        stat_up('<i class="fa-solid fa-triangle-exclamation"></i> Service Worker registration failed');
+        throw_error("Service worker failed to start: Voxity won't work offline");
+      }
+    }
+  });
+} else if (noInstall) {
+  console.log("The Service Worker will not install");
+}
