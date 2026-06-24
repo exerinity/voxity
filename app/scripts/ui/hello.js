db5d762ffc1af7e49ce4c382ce4799ad5e1e5fb3
exerinity <me@exerinity.com>
2026-06-24 21:32:55 +1000

    m

--- app/scripts/ui/hello.js
+++ app/scripts/ui/hello.js
@@ -54,7 +54,6 @@ async function loadFA() {
 function welcome() {
     const modalPromise = msg(`<p>Voxity is a web-based audio player that lets you play local audio files directly in your browser. Just drag and drop files to get started!</p>
 <p>To learn more, visit Voxity's page on my website: <a href="https://exerinity.com/projects/voxity" target="_blank" rel="noopener">https://exerinity.com/projects/voxity</a></p>
-<p><a href="https://exerinity.com/projects/voxity/screenshots" target="_blank" rel="noopener">View some screenshots of Voxity here</a></p>
 <p>Thanks, and have fun! <i class="fa-solid fa-broadcast-tower fa-beat bop"></i></p><a href="https://exerinity.com/twitter" target="_blank"><i class="fa-brands fa-twitter" style="color:#1da1f2;"></i> Follow me on Twitter</a>
 <br><small><a href="/i/reload_fa" onclick="event.preventDefault(); loadFA()">Icons are not showing...</a></small>
 `, "Welcome to Voxity");
