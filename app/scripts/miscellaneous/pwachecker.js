05cfe5f1abfef970b974f4a37345423d78a43bf7
exerinity <me@exerinity.com>
2026-07-25 17:37:08 +1000

    m

--- app/scripts/miscellaneous/pwachecker.js
+++ app/scripts/miscellaneous/pwachecker.js
@@ -17,7 +17,7 @@ async function pwamsg() {
         else {
             throw_error("The request failed, showing how-to", 2);
             const modalPromise = msg(
-                `<iframe src="/install.html" style="width:100%; height:400px; border:none; border-radius:8px;"></iframe><br><a href="/install" target="_blank" rel="noopener">Open this in new tab</a>`,
+                `<iframe src="/install.html" style="width:100%; height:400px; border:none; border-radius:8px;"></iframe><br><a href="/install" target="_blank" rel="noopener">Open this in new tab <i class="fa-solid fa-arrow-up-right-from-square"></i></a>`,
                 "Install Voxity"
             );
         }
@@ -25,7 +25,7 @@ async function pwamsg() {
     }
 
     msg(
-        `<iframe src="/install.html" style="width:100%; height:400px; border:none; border-radius:8px;"></iframe><br><a href="/install" target="_blank" rel="noopener">Open this in new tab</a>`,
+        `<iframe src="/install.html" style="width:100%; height:400px; border:none; border-radius:8px;"></iframe><br><a href="/install" target="_blank" rel="noopener">Open this in new tab <i class="fa-solid fa-arrow-up-right-from-square"></i></a>`,
         "Install Voxity"
     );
 }
